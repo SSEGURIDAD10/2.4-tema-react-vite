7111ea79c7f5e1b38433c083096d5b17a3b269e7
Villagran <luisbmx_haro@hotmail.com>
2024-05-01 22:00:10 -0600

    1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Boton from './components/Boton'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hola desde vite!</h1>
+      <Boton texto="Pizza" icon='🍕'/>
+      <Boton texto="Hamburguesa" icon='🍔'/>
+      <Boton texto="Taco" icon='🌮'/>
+    </>
+  )
+}
+
+export default App
