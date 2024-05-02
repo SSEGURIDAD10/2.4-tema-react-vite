2a48ed5ca7273911b9fff9b4a72a426c2da0b458
Villagran <luisbmx_haro@hotmail.com>
2024-05-01 23:41:29 -0600

    Se agrego el componente [titulo, setTitulo]

--- src/components/Texto.jsx
+++ src/components/Texto.jsx
@@ -4,19 +4,23 @@ import React, { Fragment, useState } from 'react'
 
 const Texto = () => {
     const [mostrar, setMostrar] = useState(false);
+    const [titulo, setTitulo] = useState('');
 
     const handlerClick = () => {
         console.log('Hola desde el boton');
         setMostrar(!mostrar);
-        console.log(mostrar);
+        setTitulo('Hola, aparezco y no me quito');
     };
 
     console.log('Estado de mostrar:', mostrar);
-    
+
   return (
     <Fragment>
         {mostrar && <h1>Titulo del componente Texto</h1>}
-        <button onClick={handlerClick}>{mostrar ? 'Ocultar': 'Mostrar'}</button>
+        <h4>{titulo}</h4>
+        <button onClick={handlerClick}>
+            {mostrar ? 'Ocultar': 'Mostrar'}
+        </button>
     </Fragment>
   )
 }
